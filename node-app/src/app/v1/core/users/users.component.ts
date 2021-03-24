1881be76cc9fc326b9dd936b0034953ba434f730
John Paul Onte <jnpl.onte@gmail.com>
2021-03-24 08:58:48 +0800

    update doc

--- node-app/src/app/v1/core/users/users.component.ts
+++ node-app/src/app/v1/core/users/users.component.ts
@@ -4,279 +4,288 @@ import { CoreMiddleware } from '../../../middlewares/core/core.middleware';
 import { UsersAttributes } from './../../../../models/users';
 
 export class Users extends CoreMiddleware {
-    pageCache: any;
-    nameCache: string = '/v1/core/user*';
+	pageCache: any;
+	nameCache: string = '/v1/core/user*';
 
-    constructor(app, cache, private response, private helper, private query) {
-        super(app, cache);
-        this.pageCache = cache;
-    }
+	constructor(app, cache, private response, private helper, private query) {
+		super(app, cache);
+		this.pageCache = cache;
+	}
 
-    get services() {
-        return {'GET /myuser': 'getMe',
-                'GET /users': 'all',
-                'GET /user/:id': 'get',
-                'POST /user': 'post',
-                'PUT /user/:id' : 'put',
-                'DELETE /user/:id' : 'delete'};
-    }
+	get services() {
+		return {
+			'GET /myuser': 'getMe',
+			'GET /users': 'all',
+			'GET /user/:id': 'get',
+			'POST /user': 'post',
+			'PUT /user/:id': 'put',
+			'DELETE /user/:id': 'delete',
+		};
+	}
 
-    get attributes(): any {
-        return { 'exclude': ['salt', 'password', 'forgotPasswordKey', 'verificationKey', 'socialMediaKey', 'createdUserId', 'updatedUserId'] };
-    }
+	get attributes(): any {
+		return {
+			exclude: [
+				'salt',
+				'password',
+				'forgotPasswordKey',
+				'verificationKey',
+				'socialMediaKey',
+				'createdUserId',
+				'updatedUserId',
+			],
+		};
+	}
 
-    /**
-     * @api {get} /core/users get all user
-     * @apiVersion 1.0.0
-     * @apiName all
-     * @apiGroup USERS
-     * @apiPermission authenticated-user
-     *
-     * @apiDescription get all user
-     *
-     * @apiParam {String} [query] filter query Ex. ?query=key:value
-     * @apiParam {Number} [limit=10] data limit Ex. ?limit=1
-     * @apiParam {Number} [page=1] page number Ex. ?page=1
-     */
-    all(req: Request, res: Response): void {
-        const whereData = {
-            'where': {
-                'active': true
-            },
-            'include': this.getInclude(req.models),
-            'attributes': this.attributes
-        };
+	/**
+	 * @api {get} /core/users get all user
+	 * @apiVersion 1.0.0
+	 * @apiName all
+	 * @apiGroup USERS
+	 * @apiPermission authenticated-user
+	 *
+	 * @apiDescription get all user
+	 *
+	 * @apiParam {String} [query] filter query Ex. ?query=key:value
+	 * @apiParam {Number} [limit=10] data limit Ex. ?limit=1
+	 * @apiParam {Number} [page=1] page number Ex. ?page=1
+	 */
+	all(req: Request, res: Response): void {
+		const whereData = {
+			where: {
+				active: true,
+			},
+			include: this.getInclude(req.models),
+			attributes: this.attributes,
+		};
 
-        return this.query.getAll(req.models.users, whereData, req.query)
-            .then(
-                (users: any) => {
-                    const {data, pagination} = users || {'data': [], 'pagination': {}};
-                    return this.response.success(res, 'get', data || [], pagination);
-                }
-            )
-            .catch(
-                (error) => this.response.failed(res, 'get', error)
-            );
-    }
+		return this.query
+			.getAll(req.models.users, whereData, req.query)
+			.then((users: any) => {
+				const { data, pagination } = users || { data: [], pagination: {} };
+				return this.response.success(res, 'get', data || [], pagination);
+			})
+			.catch((error) => this.response.failed(res, 'get', error));
+	}
 
-    /**
-     * @api {get} /core/user/:id get user
-     * @apiVersion 1.0.0
-     * @apiName get
-     * @apiGroup USERS
-     * @apiPermission authenticated-user
-     *
-     * @apiDescription get one user
-     *
-     * @apiParam (url segment) {String} id user id
-     * @apiParam (url parameter) {String} key key search Ex. ?key=name
-     */
-    get(req: Request, res: Response): void {
-        const whereData = {
-            'where': {
-                'active': true
-            },
-            'include': this.getInclude(req.models),
-            'attributes': this.attributes
-        };
+	/**
+	 * @api {get} /core/user/:id get user
+	 * @apiVersion 1.0.0
+	 * @apiName get
+	 * @apiGroup USERS
+	 * @apiPermission authenticated-user
+	 *
+	 * @apiDescription get one user
+	 *
+	 * @apiParam (url segment) {String} id user id
+	 * @apiParam (url parameter) {String} key key search Ex. ?key=name
+	 */
+	get(req: Request, res: Response): void {
+		const whereData = {
+			where: {
+				active: true,
+			},
+			include: this.getInclude(req.models),
+			attributes: this.attributes,
+		};
 
-        return this.query.getOne(req.models.users, whereData, req.query.key, req.params.id)
-            .then(
-                (user: UsersAttributes) => this.response.success(res, 'get', user || {})
-            )
-            .catch(
-                (error) => this.response.failed(res, 'get', error)
-            );
-    }
+		return this.query
+			.getOne(req.models.users, whereData, req.query.key, req.params.id)
+			.then((user: UsersAttributes) => this.response.success(res, 'get', user || {}))
+			.catch((error) => this.response.failed(res, 'get', error));
+	}
 
-    /**
-     * @api {get} /core/myuser get my user
-     * @apiVersion 1.0.0
-     * @apiName getMe
-     * @apiGroup USERS
-     * @apiPermission authenticated-user
-     *
-     * @apiDescription get my user
-     *
-     */
-    getMe(req: Request, res: Response): void {
-        const whereData = {
-            'where': {
-                'id': req.authentication.id || null,
-                'active': true
-            },
-            'include': this.getInclude(req.models),
-            'attributes': this.attributes
-        };
+	/**
+	 * @api {get} /core/myuser get my user
+	 * @apiVersion 1.0.0
+	 * @apiName getMe
+	 * @apiGroup USERS
+	 * @apiPermission authenticated-user
+	 *
+	 * @apiDescription get my user
+	 *
+	 */
+	getMe(req: Request, res: Response): void {
+		const whereData = {
+			where: {
+				id: req.authentication.id || null,
+				active: true,
+			},
+			include: this.getInclude(req.models),
+			attributes: this.attributes,
+		};
 
-        return this.query.getOne(req.models.users, whereData)
-            .then(
-                (user: UsersAttributes) => this.response.success(res, 'get', user || {})
-            )
-            .catch(
-                (error) => this.response.failed(res, 'get', error)
-            );
-    }
+		return this.query
+			.getOne(req.models.users, whereData)
+			.then((user: UsersAttributes) => this.response.success(res, 'get', user || {}))
+			.catch((error) => this.response.failed(res, 'get', error));
+	}
 
-    /**
-     * @api {post} /core/user insert user
-     * @apiVersion 1.0.0
-     * @apiName post
-     * @apiGroup USERS
-     * @apiPermission admin-user
-     *
-     * @apiDescription insert user
-     *
-     * @apiParam (body) {String} firstName first name
-     * @apiParam (body) {String} lastName last name
-     * @apiParam (body) {String} email unique email address
-     * @apiParam (body) {String} username unique user name
-     * @apiParam (body) {String} password MD5 hash password
-     * @apiParam (body) {String} [phone] contact number
-     * @apiParam (body) {String} [socialMedia=NONE] social media <br /> Expected Value: `FACEBOOK|INSTAGRAM|TWITTER`
-     * @apiParam (body) {String} [socialMediaKey] social media key
-     * @apiParam (body) {Number} [languageId] language id
-     * @apiParam (body) {Number} [roleId] role id
-     */
-    post(req: Request, res: Response): void {
-        const reqParameters = ['firstName', 'lastName', 'email', 'username', 'password'];
-        if (!this.helper.validateData(req.body, reqParameters)) {
-            return this.response.failed(res, 'data', reqParameters);
-        }
+	/**
+	 * @api {post} /core/user insert user
+	 * @apiVersion 1.0.0
+	 * @apiName post
+	 * @apiGroup USERS
+	 * @apiPermission authenticated-user
+	 *
+	 * @apiDescription insert user
+	 *
+	 * @apiParam (body) {String} firstName first name
+	 * @apiParam (body) {String} lastName last name
+	 * @apiParam (body) {String} email unique email address
+	 * @apiParam (body) {String} username unique user name
+	 * @apiParam (body) {String} password MD5 hash password
+	 * @apiParam (body) {String} [phone] contact number
+	 * @apiParam (body) {String} [socialMedia=NONE] social media <br /> Expected Value: `FACEBOOK|INSTAGRAM|TWITTER`
+	 * @apiParam (body) {String} [socialMediaKey] social media key
+	 * @apiParam (body) {Number} [languageId] language id
+	 * @apiParam (body) {Number} [roleId] role id
+	 */
+	post(req: Request, res: Response): void {
+		const reqParameters = ['firstName', 'lastName', 'email', 'username', 'password'];
+		if (!this.helper.validateData(req.body, reqParameters)) {
+			return this.response.failed(res, 'data', reqParameters);
+		}
 
-        const data = this.autoFillPostInformation(req.body, req.authentication);
+		const data = this.autoFillPostInformation(req.body, req.authentication);
 
-        return this.query.post(req.models.users, data)
-            .then(
-                (user: UsersAttributes) => this.cacheCheck(this.pageCache, this.nameCache)((user) ? this.response.success(res, 'post', user.id) : this.response.failed(res, 'post'))
-            )
-            .catch(
-                (error) => this.response.failed(res, 'post', error)
-            );
-    }
+		return this.query
+			.post(req.models.users, data)
+			.then((user: UsersAttributes) =>
+				this.cacheCheck(
+					this.pageCache,
+					this.nameCache
+				)(user ? this.response.success(res, 'post', user.id) : this.response.failed(res, 'post'))
+			)
+			.catch((error) => this.response.failed(res, 'post', error));
+	}
 
-    /**
-     * @api {put} /core/user/:id update user
-     * @apiVersion 1.0.0
-     * @apiName put
-     * @apiGroup USERS
-     * @apiPermission authenticated-user
-     *
-     * @apiDescription update user
-     *
-     * @apiParam (url segment) {String} id user id
-     * @apiParam (body) {String} [firstName] first name
-     * @apiParam (body) {String} [lastName] last name
-     * @apiParam (body) {String} [email] unique email address
-     * @apiParam (body) {String} [password] MD5 hash password
-     * @apiParam (body) {String} [phone] contact number
-     * @apiParam (body) {String} [socialMedia=NONE] social media <br /> Expected Value: `FACEBOOK|INSTAGRAM|TWITTER`
-     * @apiParam (body) {String} [socialMediaKey] social media key
-     * @apiParam (body) {Number} [languageId] language id
-     * @apiParam (body) {Number} [currencyId] currency id
-     * @apiParam (body) {Boolean} [verified] is user verified
-     * @apiParam (body) {Boolean} [active] is user active
-     */
-    put(req: Request, res: Response): void {
-        const id = req.params.id;
+	/**
+	 * @api {put} /core/user/:id update user
+	 * @apiVersion 1.0.0
+	 * @apiName put
+	 * @apiGroup USERS
+	 * @apiPermission authenticated-user
+	 *
+	 * @apiDescription update user
+	 *
+	 * @apiParam (url segment) {String} id user id
+	 * @apiParam (body) {String} [firstName] first name
+	 * @apiParam (body) {String} [lastName] last name
+	 * @apiParam (body) {String} [email] unique email address
+	 * @apiParam (body) {String} [password] MD5 hash password
+	 * @apiParam (body) {String} [phone] contact number
+	 * @apiParam (body) {String} [socialMedia=NONE] social media <br /> Expected Value: `FACEBOOK|INSTAGRAM|TWITTER`
+	 * @apiParam (body) {String} [socialMediaKey] social media key
+	 * @apiParam (body) {Number} [languageId] language id
+	 * @apiParam (body) {Number} [currencyId] currency id
+	 * @apiParam (body) {Boolean} [verified] is user verified
+	 * @apiParam (body) {Boolean} [active] is user active
+	 */
+	put(req: Request, res: Response): void {
+		const id = req.params.id;
 
-        const whereData = {
-            'where': {
-                'id': id
-            }
-        };
+		const whereData = {
+			where: {
+				id: id,
+			},
+		};
 
-        const data = this.autoFillPutInformation(req.body, req.authentication);
+		const data = this.autoFillPutInformation(req.body, req.authentication);
 
-        return this.query.put(req.models.users, data, whereData)
-            .then(
-                (user: number[]) => this.cacheCheck(this.pageCache, this.nameCache)((user[0]) ? this.response.success(res, 'put', id) : this.response.failed(res, 'put'))
-            )
-            .catch(
-                (error) => this.response.failed(res, 'put', error)
-            );
-    }
+		return this.query
+			.put(req.models.users, data, whereData)
+			.then((user: number[]) =>
+				this.cacheCheck(
+					this.pageCache,
+					this.nameCache
+				)(user[0] ? this.response.success(res, 'put', id) : this.response.failed(res, 'put'))
+			)
+			.catch((error) => this.response.failed(res, 'put', error));
+	}
 
-    /**
-     * @api {delete} /core/user/:id delete user
-     * @apiVersion 1.0.0
-     * @apiName delete
-     * @apiGroup USERS
-     * @apiPermission authenticated-user
-     *
-     * @apiDescription delete user
-     *
-     * @apiParam (url segment) {String} id user id
-     */
-    delete(req: Request, res: Response): void {
-        const id = req.params.id;
-        const whereData = {
-            'where': {
-                'id': id
-            }
-        };
+	/**
+	 * @api {delete} /core/user/:id delete user
+	 * @apiVersion 1.0.0
+	 * @apiName delete
+	 * @apiGroup USERS
+	 * @apiPermission authenticated-user
+	 *
+	 * @apiDescription delete user
+	 *
+	 * @apiParam (url segment) {String} id user id
+	 */
+	delete(req: Request, res: Response): void {
+		const id = req.params.id;
+		const whereData = {
+			where: {
+				id: id,
+			},
+		};
 
-        return this.query.put(req.models.users, {'active': false, 'updatedUserId': req.authentication.id || null}, whereData)
-            .then(
-                (users: number[]) => this.cacheCheck(this.pageCache, this.nameCache)((users[0]) ? this.response.success(res, 'delete', id) : this.response.failed(res, 'delete'))
-            )
-            .catch(
-                (error) => this.response.failed(res, 'delete', error)
-            );
-    }
+		return this.query
+			.put(req.models.users, { active: false, updatedUserId: req.authentication.id || null }, whereData)
+			.then((users: number[]) =>
+				this.cacheCheck(
+					this.pageCache,
+					this.nameCache
+				)(users[0] ? this.response.success(res, 'delete', id) : this.response.failed(res, 'delete'))
+			)
+			.catch((error) => this.response.failed(res, 'delete', error));
+	}
 
-    protected getInclude(model: any): Array<any> {
-        const includedData: Array<any> = [
-            {
-                'model': model.roles,
-                'required': true,
-                'attributes': ['id', 'name', 'permissionLevel']
-            },
-            {
-                'model': model.languages,
-                'required': false,
-                'attributes': ['id', 'code', 'name']
-            },
-            {
-                'model': model.countries,
-                'required': false,
-                'attributes': ['id', 'code', 'name']
-            }
-        ];
+	protected getInclude(model: any): Array<any> {
+		const includedData: Array<any> = [
+			{
+				model: model.roles,
+				required: true,
+				attributes: ['id', 'name', 'permissionLevel'],
+			},
+			{
+				model: model.languages,
+				required: false,
+				attributes: ['id', 'code', 'name'],
+			},
+			{
+				model: model.countries,
+				required: false,
+				attributes: ['id', 'code', 'name'],
+			},
+		];
 
-        return includedData;
-    }
+		return includedData;
+	}
 
-    protected autoFillPostInformation(data: UsersAttributes, authData: Object = {}) {
-        if (typeof(data.password) !== 'undefined') {
-            data.salt = this.helper.generateRandomString();
-            data.password = this.helper.getPassword(data.password, data.salt);
-            data.passwordExpiry = this.helper.passwordExpiry;
-        }
+	protected autoFillPostInformation(data: UsersAttributes, authData: Object = {}) {
+		if (typeof data.password !== 'undefined') {
+			data.salt = this.helper.generateRandomString();
+			data.password = this.helper.getPassword(data.password, data.salt);
+			data.passwordExpiry = this.helper.passwordExpiry;
+		}
 
-        data.roleId = (data.roleId) ? Number(data.roleId) : this.helper.defaultUserRole;
+		data.roleId = data.roleId ? Number(data.roleId) : this.helper.defaultUserRole;
 
-        if (typeof(authData['id']) !== 'undefined') {
-            data.createdUserId = authData['id'];
-            data.updatedUserId = authData['id'];
-        }
+		if (typeof authData['id'] !== 'undefined') {
+			data.createdUserId = authData['id'];
+			data.updatedUserId = authData['id'];
+		}
 
-        return data;
-    }
+		return data;
+	}
 
-    protected autoFillPutInformation(data: UsersAttributes, authData: Object = {}): Object {
-        if (typeof(data.password) !== 'undefined') {
-            data.salt = this.helper.generateRandomString();
-            data.password = this.helper.getPassword(data.password, data.salt);
-            data.passwordExpiry = this.helper.passwordExpiry;
-        }
+	protected autoFillPutInformation(data: UsersAttributes, authData: Object = {}): Object {
+		if (typeof data.password !== 'undefined') {
+			data.salt = this.helper.generateRandomString();
+			data.password = this.helper.getPassword(data.password, data.salt);
+			data.passwordExpiry = this.helper.passwordExpiry;
+		}
 
-        if (typeof(authData['id']) !== 'undefined') {
-            data.updatedUserId = authData['id'];
-        }
+		if (typeof authData['id'] !== 'undefined') {
+			data.updatedUserId = authData['id'];
+		}
 
-        delete data.username;
+		delete data.username;
 
-        return data;
-    }
+		return data;
+	}
 }
--- node-app/src/app/v1/core/users/users.spec.ts
+++ node-app/src/app/v1/core/users/users.spec.ts
@@ -7,131 +7,156 @@ import { Users } from './users.component';
 import { Helper } from './../../../../app/services/helper/helper.service';
 import { baseConfig } from './../../../../config';
 
-const authKey: string = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImRhNzQ0YTdlLWMwNTgtNDA4Mi1iYTNlLWQ1MmYzNjI5MzY5OCIsInVzZXJuYW1lIjoic3VwZXJBZG1pbiIsImZpcnN0TmFtZSI6IlN1cGVyIiwibGFzdE5hbWUiOiJBZG1pbiIsInJvbGVJZCI6MSwibG9naW5Db3VudCI6MCwicGVybWlzc2lvbkxldmVsIjoxfQ.IsTx_7ru52zlzk8af1mcSUh2Atsnv677dumVbnRptpU';
+const authKey: string =
+	'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImRhNzQ0YTdlLWMwNTgtNDA4Mi1iYTNlLWQ1MmYzNjI5MzY5OCIsInVzZXJuYW1lIjoic3VwZXJBZG1pbiIsImZpcnN0TmFtZSI6IlN1cGVyIiwibGFzdE5hbWUiOiJBZG1pbiIsInJvbGVJZCI6MSwibG9naW5Db3VudCI6MCwicGVybWlzc2lvbkxldmVsIjoxfQ.IsTx_7ru52zlzk8af1mcSUh2Atsnv677dumVbnRptpU';
 let testName: number = 1;
 
 const timeStamp: number = new Date().getTime();
 describe('users component', () => {
-    let application: any;
+	let application: any;
 
-    before( (done) => {
-        application = new Users('sandbox', null, null, new Helper(baseConfig), null);
+	before((done) => {
+		application = new Users('sandbox', null, null, new Helper(baseConfig), null);
 
-        done();
-    });
+		done();
+	});
 
-    it('should have the basic method', (done) => {
-        expect(application.autoFillPostInformation).to.exist;
-        expect(application.autoFillPostInformation).to.be.a('function');
-        expect(application.autoFillPutInformation).to.exist;
-        expect(application.autoFillPutInformation).to.be.a('function');
-        expect(application.postInclude).to.exist;
-        expect(application.postInclude).to.be.a('function');
+	it('should have the basic method', (done) => {
+		expect(application.autoFillPostInformation).to.exist;
+		expect(application.autoFillPostInformation).to.be.a('function');
+		expect(application.autoFillPutInformation).to.exist;
+		expect(application.autoFillPutInformation).to.be.a('function');
+		expect(application.postInclude).to.exist;
+		expect(application.postInclude).to.be.a('function');
 
-        done();
-    });
+		done();
+	});
 
-    it('should fetch post and put information', (done) => {
-        const data: Object = {
-            'firstName': 'test'
-        };
-        const dataPassword: Object = {
-            'firstName': 'test',
-            'password': 'password'
-        };
+	it('should fetch post and put information', (done) => {
+		const data: Object = {
+			firstName: 'test',
+		};
+		const dataPassword: Object = {
+			firstName: 'test',
+			password: 'password',
+		};
 
-        const auth: Object = {
-            'id': 'abc',
-        };
+		const auth: Object = {
+			id: 'abc',
+		};
 
-        expect(application.autoFillPostInformation(dataPassword, auth)).to.contain.keys('salt', 'password', 'passwordExpiry', 'roleId');
+		expect(application.autoFillPostInformation(dataPassword, auth)).to.contain.keys(
+			'salt',
+			'password',
+			'passwordExpiry',
+			'roleId'
+		);
 
-        expect(application.autoFillPutInformation(data, auth)).to.eql({
-            'firstName': 'test',
-            'updatedUserId': 'abc'
-        });
-        expect(application.autoFillPutInformation(dataPassword, auth)).to.contain.keys('salt', 'password', 'passwordExpiry');
+		expect(application.autoFillPutInformation(data, auth)).to.eql({
+			firstName: 'test',
+			updatedUserId: 'abc',
+		});
+		expect(application.autoFillPutInformation(dataPassword, auth)).to.contain.keys(
+			'salt',
+			'password',
+			'passwordExpiry'
+		);
 
-        done();
-    });
+		done();
+	});
 
-    it('should get all user', (done) => {
-        supertest(app).get('/v1/core/users?test=true')
-            .set('Authorization', `Bearer ${authKey}`)
-            .expect('Content-Type', /json/)
-            .expect(200, (err, res) => {
-                if (err) { return done(err); }
+	it('should get all user', (done) => {
+		supertest(app)
+			.get('/v1/core/users?test=true')
+			.set('Authorization', `Bearer ${authKey}`)
+			.expect('Content-Type', /json/)
+			.expect(200, (err, res) => {
+				if (err) {
+					return done(err);
+				}
 
-                expect(res.body.status).to.equal('success');
-                expect(res.body.data).to.be.a('Array');
-                expect(res.body.data).to.have.lengthOf.above(1);
+				expect(res.body.status).to.equal('success');
+				expect(res.body.data).to.be.a('Array');
+				expect(res.body.data).to.have.lengthOf.above(1);
 
-                done();
-            });
-    });
+				done();
+			});
+	});
 
-    it('should create user', (done) => {
-        supertest(app).post('/v1/core/user?test=true')
-            .set('Authorization', `Bearer ${authKey}`)
-            .send({
-                'firstName': 'test',
-                'lastName': 'test',
-                'email': `test${timeStamp}@email.com`,
-                'username': `test${timeStamp}`,
-                'password': '5f4dcc3b5aa765d61d8327deb882cf99'
-            })
-            .expect('Content-Type', /json/)
-            .expect(200, (err, res) => {
-                if (err) { return done(err); }
+	it('should create user', (done) => {
+		supertest(app)
+			.post('/v1/core/user?test=true')
+			.set('Authorization', `Bearer ${authKey}`)
+			.send({
+				firstName: 'test',
+				lastName: 'test',
+				email: `test${timeStamp}@email.com`,
+				username: `test${timeStamp}`,
+				password: '5f4dcc3b5aa765d61d8327deb882cf99',
+			})
+			.expect('Content-Type', /json/)
+			.expect(200, (err, res) => {
+				if (err) {
+					return done(err);
+				}
 
-                testName = res.body.data || 0;
-                expect(res.body.status).to.equal('success');
+				testName = res.body.data || 0;
+				expect(res.body.status).to.equal('success');
 
-                done();
-            });
-    });
+				done();
+			});
+	});
 
-    it('should get user', (done) => {
-        supertest(app).get(`/v1/core/user/${testName}?test=true`)
-            .set('Authorization', `Bearer ${authKey}`)
-            .expect('Content-Type', /json/)
-            .expect(200, (err, res) => {
-                if (err) { return done(err); }
+	it('should get user', (done) => {
+		supertest(app)
+			.get(`/v1/core/user/${testName}?test=true`)
+			.set('Authorization', `Bearer ${authKey}`)
+			.expect('Content-Type', /json/)
+			.expect(200, (err, res) => {
+				if (err) {
+					return done(err);
+				}
 
-                expect(res.body.status).to.equal('success');
-                expect(res.body.data).to.be.a('Object');
+				expect(res.body.status).to.equal('success');
+				expect(res.body.data).to.be.a('Object');
 
-                done();
-            });
-    });
+				done();
+			});
+	});
 
-    it('should update user', (done) => {
-        supertest(app).put(`/v1/core/user/${testName}?test=true`)
-            .set('Authorization', `Bearer ${authKey}`)
-            .send({
-                'firstName': 'test-update',
-                'lastName': 'test-update'
-            })
-            .expect('Content-Type', /json/)
-            .expect(200, (err, res) => {
-                if (err) { return done(err); }
+	it('should update user', (done) => {
+		supertest(app)
+			.put(`/v1/core/user/${testName}?test=true`)
+			.set('Authorization', `Bearer ${authKey}`)
+			.send({
+				firstName: 'test-update',
+				lastName: 'test-update',
+			})
+			.expect('Content-Type', /json/)
+			.expect(200, (err, res) => {
+				if (err) {
+					return done(err);
+				}
 
-                expect(res.body.status).to.equal('success');
+				expect(res.body.status).to.equal('success');
 
-                done();
-            });
-    });
+				done();
+			});
+	});
 
-    it('should delete user', (done) => {
-        supertest(app).delete(`/v1/core/user/${testName}?test=true`)
-            .set('Authorization', `Bearer ${authKey}`)
-            .expect('Content-Type', /json/)
-            .expect(200, (err, res) => {
-                if (err) { return done(err); }
+	it('should delete user', (done) => {
+		supertest(app)
+			.delete(`/v1/core/user/${testName}?test=true`)
+			.set('Authorization', `Bearer ${authKey}`)
+			.expect('Content-Type', /json/)
+			.expect(200, (err, res) => {
+				if (err) {
+					return done(err);
+				}
 
-                expect(res.body.status).to.equal('success');
+				expect(res.body.status).to.equal('success');
 
-                done();
-            });
-    });
+				done();
+			});
+	});
 });
